cacd4534a56907e51cf2faf3d66ff535d5f9b0bd
Zak <zakaria.laa@hotmail.fr>
2021-02-10 20:34:12 +0100

    change variable name in generate_data

--- generate_data.py
+++ generate_data.py
@@ -3,12 +3,7 @@ import numpy as np
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
-<<<<<<< HEAD
 mu, sigma = 100, 15
 x_gaussian = mu + sigma * np.random.randn(10000)
-=======
-mu, sigma = 100, 5
-x = mu + sigma * (np.random.randn(10,10000) ** 2).sum(0)
->>>>>>> 47fb5f2 (Data generation is no longer Gaussian!)
 
 np.savetxt('file_data.csv', x_gaussian, delimiter=',')
